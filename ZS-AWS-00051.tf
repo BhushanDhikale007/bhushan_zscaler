733f669062898b8d625fedda269db64db39fdf1b
BhushanDhikale007 <101262292+BhushanDhikale007@users.noreply.github.com>
2022-12-13 15:31:59 +0530

    Update ZS-AWS-00051.tf

--- ZS-AWS-00051.tf
+++ ZS-AWS-00051.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "example" {
     from_port        = 22
     to_port          = 3306
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_blocks]
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
   }
 }
